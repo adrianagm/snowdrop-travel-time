ce41f34a4efcef247e99ebac18df3d54e0f977d1
Javi Santos <javi@snowdropsolutions.co.uk>
2015-02-12 15:05:38 +0100

    rename module

--- js/controls/picture-exportation-control.js
+++ js/controls/picture-exportation-control.js
@@ -2,8 +2,8 @@
 
     MapViewer.ButtonControl = MapViewer.extend(MapViewer.MapControl, {
 
-        template: '<div class="button-control-outer"><div class="button-control-border"><div class="button-control-inner"><b>Picture Exportation</b></div></div></div>',
-        controlClass: 'button-control',
+        template: '<div class="picture-exportation-control-outer"><div class="picture-exportation-control-border"><div class="picture-exportation-control-inner"><b>Picture Exportation</b></div></div></div>',
+        controlClass: 'picture-exportation-control',
 
         position: 'TOP_RIGHT',
         alias: 'button',
@@ -16,78 +16,83 @@
         bounds: null,
         zoom: null,
         initialize: function() {
-            this.buttonText = this.getElementsByClass('button-control-inner')[0];
+            this.buttonText = this.getElementsByClass('picture-exportation-control-inner')[0];
             this.elem = this.map.getDiv();
             var that = this;
-
-            this.bindEvent('button-control-outer', 'click', function() {
-                this.controls = document.getElementsByClassName('map-control');
+            this.bindEvent('picture-exportation-control-outer', 'click', function() {
 
                 if (this.classList.contains("complete-screen")) {
                     this.classList.remove('complete-screen');
-                    that.complete = false;
+                    that.deactivate();
                 } else {
                     this.classList.add('complete-screen');
-                    that.complete = true;
-                }
-
-                if (that.complete) {
-                    //Complete screen
-                    that.buttonText.innerHTML = "<b>X</b>";
-                    that.map.set('disableDefaultUI', true);
-                    that.map.set('mapTypeControl', false);
-                    for (var i = 0; i < this.controls.length; i++) {
-                        if (!this.controls[i].firstChild.classList.contains('button-control'))
-                            this.controls[i].style.display = "none";
-                    }
-
-                    that.elem.style.width = "100%";
-                    that.bounds = that.map.getBounds();
-                    that.zoom = that.map.getZoom();
-
-                    if (that.elem.requestFullscreen) {
-                        that.elem.requestFullscreen();
-                    } else if (that.elem.msRequestFullscreen) {
-                        that.elem.msRequestFullscreen();
-                    } else if (that.elem.mozRequestFullScreen) {
-                        that.elem.mozRequestFullScreen();
-                    } else if (that.elem.webkitRequestFullscreen) {
-                        that.elem.webkitRequestFullscreen();
-                    }
-
-                    setTimeout(function() {
-                        that.map.fitBounds(that.bounds);
-                        that.map.setZoom(that.zoom);
-                    }, 100);
-
-                } else {
-                    //Normal screen
-                    that.buttonText.innerHTML = "<b>Picture Exportation</b>";
-                    that.map.set('disableDefaultUI', false);
-                    that.map.set('mapTypeControl', true);
-
-                    that.elem.style.width = "initial";
-                    for (var j = 0; j < this.controls.length; j++) {
-                        if (!this.controls[j].firstChild.classList.contains('button-control'))
-                            this.controls[j].style.display = "initial";
-                    }
-                    if (document.exitFullscreen) {
-                        document.exitFullscreen();
-                    } else if (document.msExitFullscreen) {
-                        document.msExitFullscreen();
-                    } else if (document.mozCancelFullScreen) {
-                        document.mozCancelFullScreen();
-                    } else if (document.webkitExitFullscreen) {
-                        document.webkitExitFullscreen();
-                    }
-                    setTimeout(function() {
-                        that.map.fitBounds(that.bounds);
-                        that.map.setZoom(that.zoom);
-                    }, 100);
+                    that.activate();
                 }
             });
+        },
+
+
+        deactivate: function() {
+
+            var that = this;
+            //Normal screen
+            that.buttonText.innerHTML = "<b>Picture Exportation</b>";
+            that.map.set('disableDefaultUI', false);
+            that.map.set('mapTypeControl', true);
+
+            that.elem.style.width = "initial";
+            for (var j = 0; j < this.controls.length; j++) {
+                if (!this.controls[j].firstChild.classList.contains('picture-exportation-control'))
+                    this.controls[j].style.display = "initial";
+            }
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+            setTimeout(function() {
+                that.map.fitBounds(that.bounds);
+                that.map.setZoom(that.zoom);
+            }, 100);
+        },
+
+        activate: function() {
+            this.controls = document.getElementsByClassName('map-control');
+            
+            var that = this;
+            //Complete screen
+            that.buttonText.innerHTML = "<b>X</b>";
+            that.map.set('disableDefaultUI', true);
+            that.map.set('mapTypeControl', false);
+            for (var i = 0; i < this.controls.length; i++) {
+                if (!this.controls[i].firstChild.classList.contains('picture-exportation-control'))
+                    this.controls[i].style.display = "none";
+            }
+
+            that.elem.style.width = "100%";
+            that.bounds = that.map.getBounds();
+            that.zoom = that.map.getZoom();
+
+            if (that.elem.requestFullscreen) {
+                that.elem.requestFullscreen();
+            } else if (that.elem.msRequestFullscreen) {
+                that.elem.msRequestFullscreen();
+            } else if (that.elem.mozRequestFullScreen) {
+                that.elem.mozRequestFullScreen();
+            } else if (that.elem.webkitRequestFullscreen) {
+                that.elem.webkitRequestFullscreen();
+            }
+
+            setTimeout(function() {
+                that.map.fitBounds(that.bounds);
+                that.map.setZoom(that.zoom);
+            }, 100);
         }
     });
 
-    MapViewer.registerModule(MapViewer.ButtonControl, "button");
+    MapViewer.registerModule(MapViewer.ButtonControl, "picture-exportation");
 })();
--- test/map-viewer-test.js
+++ test/map-viewer-test.js
@@ -2,140 +2,140 @@ var mapViewer;
 
 var IntegrationAPI = {
 
-	addSearchListener: function(callback) {
-		this.callback = callback;
-	},
+    addSearchListener: function(callback) {
+        this.callback = callback;
+    },
 
-	searchByPolygon: function(poligonPoints) {
-		var list = [];
-		var bounds = [];
+    searchByPolygon: function(poligonPoints) {
+        var list = [];
+        var bounds = [];
 
-		var minLat = poligonPoints[0].lat();
-		var maxLat = poligonPoints[0].lat();
-		var minLng = poligonPoints[0].lng();
-		var maxLng = poligonPoints[0].lng();
+        var minLat = poligonPoints[0].lat();
+        var maxLat = poligonPoints[0].lat();
+        var minLng = poligonPoints[0].lng();
+        var maxLng = poligonPoints[0].lng();
 
-		for (var i = 0; i < poligonPoints.length; i++) {
-			if (poligonPoints[i].lat() < minLat)
-				minLat = poligonPoints[i].lat();
+        for (var i = 0; i < poligonPoints.length; i++) {
+            if (poligonPoints[i].lat() < minLat)
+                minLat = poligonPoints[i].lat();
 
-			if (poligonPoints[i].lat() > maxLat)
-				maxLat = poligonPoints[i].lat();
+            if (poligonPoints[i].lat() > maxLat)
+                maxLat = poligonPoints[i].lat();
 
-			if (poligonPoints[i].lng() < minLng)
-				minLng = poligonPoints[i].lng();
+            if (poligonPoints[i].lng() < minLng)
+                minLng = poligonPoints[i].lng();
 
-			if (poligonPoints[i].lng() > maxLng)
-				maxLng = poligonPoints[i].lng();
-		}
+            if (poligonPoints[i].lng() > maxLng)
+                maxLng = poligonPoints[i].lng();
+        }
 
-		var poligon = new google.maps.Polygon({
-			paths: poligonPoints
-		});
+        var poligon = new google.maps.Polygon({
+            paths: poligonPoints
+        });
 
-		for (var j = 0; j < 20; j++) {
-			var lat = RandomCoordinate(minLat, maxLat);
-			var lng = RandomCoordinate(minLng, maxLng);
+        for (var j = 0; j < 20; j++) {
+            var lat = RandomCoordinate(minLat, maxLat);
+            var lng = RandomCoordinate(minLng, maxLng);
 
-			var elem = {
-				propertyId: 1,
-				lat: lat,
-				lng: lng,
-				fuzzy: false,
-				type: "test"
-			};
+            var elem = {
+                propertyId: 1,
+                lat: lat,
+                lng: lng,
+                fuzzy: false,
+                type: "test"
+            };
 
-			var point = new google.maps.LatLng(lat, lng);
+            var point = new google.maps.LatLng(lat, lng);
 
-			if (poligonPoints.length != 2) {
-				if (google.maps.geometry.poly.containsLocation(point, poligon)) {
-					list.push(elem);
-				}
-			} else {
-				list.push(elem);
-			}
-		}
+            if (poligonPoints.length != 2) {
+                if (google.maps.geometry.poly.containsLocation(point, poligon)) {
+                    list.push(elem);
+                }
+            } else {
+                list.push(elem);
+            }
+        }
 
-		var that = this;
-		setTimeout(function() {
-			that.callback(list);
-		}, 500);
+        var that = this;
+        setTimeout(function() {
+            that.callback(list);
+        }, 500);
 
-		function RandomCoordinate(min, max) {
-			return Math.random() * (max - min) + min;
-		}
-	},
+        function RandomCoordinate(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+    },
 
-	setPropertiesFilter: function(propertiesIds) {
+    setPropertiesFilter: function(propertiesIds) {
 
-	}
+    }
 };
 
 function hide() {
-	var controls = document.getElementsByClassName("button-control");
-	for (var i = 0; i < controls.length; i++) {
-		var control = controls[i];
-		if (control.classList.contains('nhide')) {
-			continue;
-		}
-		if (control.style.display !== 'none') {
-			control.style.display = 'none';
-		} else {
-			control.style.display = 'initial';
-		}
-	}
+    var controls = document.getElementsByClassName("button-control");
+    for (var i = 0; i < controls.length; i++) {
+        var control = controls[i];
+        if (control.classList.contains('nhide')) {
+            continue;
+        }
+        if (control.style.display !== 'none') {
+            control.style.display = 'none';
+        } else {
+            control.style.display = 'initial';
+        }
+    }
 }
 
 function MapViewerTest() {
-	console.log("Map Viewer");
+    console.log("Map Viewer");
 
-	var placesToolbar = {
-		type: 'places',
-		places: {
-			Schools: {
-				type: 'school' //default icon
-			},
-			Hospitals: {
-				type: 'hospital',
-				iconClass: 'fa fa-hospital-o fa-2x' //fontawesome icon
-			},
-			Supermarkets: {
-				type: 'grocery_or_supermarket' //css defined icon
-			},
-			Restaurants: {
-				type: 'restaurant',
-				iconClass: "fa fa-cutlery fa-2x"
-			}
-		}
-	};
+    var placesToolbar = {
+        type: 'places',
+        places: {
+            Schools: {
+                type: 'school' //default icon
+            },
+            Hospitals: {
+                type: 'hospital',
+                iconClass: 'fa fa-hospital-o fa-2x' //fontawesome icon
+            },
+            Supermarkets: {
+                type: 'grocery_or_supermarket' //css defined icon
+            },
+            Restaurants: {
+                type: 'restaurant',
+                iconClass: "fa fa-cutlery fa-2x"
+            }
+        }
+    };
 
-	var layerListControl = {
-		type: 'layer-list',
-		layers: {
-			"GME Layer": {
-				type: "gme",
-				layerId: "10446176163891957399-13516001307527776624",
-				layerName: 'layer_00001'
-			},
-			"GME Layer 2": {
-				type: "gme",
-				layerId: "10446176163891957399-13516001307527776624",
-				layerName: 'layer_00002'
-			},
-			"WMS Layer states": {
-				type: "wms",
-				url: "http://webservices.nationalatlas.gov/wms",
-				layers: 'states'
-			},
-		}
-	};
+    var layerListControl = {
+        type: 'layer-list',
+        layers: {
+            "GME Layer": {
+                type: "gme",
+                layerId: "10446176163891957399-13516001307527776624",
+                layerName: 'layer_00001'
+            },
+            "GME Layer 2": {
+                type: "gme",
+                layerId: "10446176163891957399-13516001307527776624",
+                layerName: 'layer_00002'
+            },
+            "WMS Layer states": {
+                type: "wms",
+                url: "http://webservices.nationalatlas.gov/wms",
+                layers: 'states'
+            },
+        }
+    };
 
-	var mapViewer = new MapViewer('map', IntegrationAPI, [
-		layerListControl,
-		placesToolbar,
-		"search-on-pan", "check-draw"
+    var mapViewer = new MapViewer('map', IntegrationAPI, [
+        layerListControl,
+        placesToolbar,
+        "search-on-pan", "check-draw", 'picture-exportation'
 
-	]);
+    ]);
 }
 
 window.onload = MapViewerTest;
